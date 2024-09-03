9c80749106d5ce80423b7f15052f9730d83bd6e1
AchmadDanniMPA <gundul321mmm@gmail.com>
2024-09-03 12:51:25 +0700

    first commit

---
+++ P2/a_danni_mpa_01_p1/bin/a_danni_mpa_01_p1.dart
@@ -0,0 +1,7 @@
+void main() {
+  var a = 18;
+  for (int i = 0; i < 10; i++) {
+    print('Halo nama aing Danni, saya berumur sekitar $a tahun');
+    a=a-1;
+  }
+}
---
+++ P2/a_danni_mpa_01_p1/test/a_danni_mpa_01_p1_test.dart
@@ -0,0 +1,8 @@
+import 'package:a_danni_mpa_01_p1/a_danni_mpa_01_p1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
